7a9080db99103fbfba2632ec8e6323d2b17cdd7a
franko <fstipanov@gmail.com>
2023-04-28 09:05:01 +0200

    finished normal questions

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,10 @@ import SingleQuestion from "./SingleQuestion";
 
 const App = () => {
   const [questions, setQuestions] = useState(data);
-  console.log(data);
   return <main>
     <section className="container">
       <h1>Questions</h1>
       {questions.map(question => <SingleQuestion key={question.id}{...question} />)}
-
     </section>
   </main>
 };
--- src/SingleQuestion.jsx
+++ src/SingleQuestion.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import { FaPlus, FaMinus } from "react-icons/fa";
+
 const SingleQuestion = ({ title, info }) => {
     const [showAnswer, setShowAnswer] = useState(false);
     return (
         <article className="question">
-
             <header>
                 <h5>{title}</h5>
-                <button type="button " className="question-btn" onClick={() => setShowAnswer(!showAnswer)}>
+                <button type="button"
+                    className="question-btn"
+                    onClick={() => setShowAnswer(!showAnswer)}>
                     {showAnswer ? <FaMinus /> : <FaPlus />}
-
                 </button>
             </header >
             {showAnswer && <p>{info}</p>}
